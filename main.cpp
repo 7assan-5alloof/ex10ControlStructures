881686bba8ccbf3dac2d116c146c968174f7d740
Hassan Khalloof <hassankhalloof20@gmail.com>
2020-12-24 15:09:24 +0400

    Complete project (#2)

--- main.cpp
+++ main.cpp
@@ -4,6 +4,23 @@ using namespace std;
 
 int main()
 {
-    cout << "Hello world!" << endl;
+    cout << "Press Ctrl-C to stop" << endl;
+
+    while (true)
+    {
+        int year;
+        cout << "Enter a year: ";
+        cin >> year;
+
+        bool leapYear;
+        if (year % 400 == 0 || year % 4 == 0 && year % 100 != 0) leapYear = true;
+        else leapYear = false;
+
+        cout << year << " is ";
+        if (leapYear);
+        else cout << "not ";
+        cout << "a leap year" << endl;
+    }
+
     return 0;
 }
